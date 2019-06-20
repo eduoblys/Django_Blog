d835551a4946adeaf24df215fc2acac26094dee8
eduoblys <eduoblys@gmail.com>
2019-06-20 09:48:26 +0200

    branch

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Post
 
-posts = [
-    {
-        'author' : 'Egis',
-        'title' : 'Blog Post 1',
-        'content' : 'First post ever',
-        'date_posted' : 'August 27, 1998',
-    },
-    {
-        'author' : 'Jane',
-        'title' : 'Blog Post 2',
-        'content' : 'Second post ever',
-        'date_posted' : 'September 27, 2005',
-    } 
-]
 
 def home(request):
     context = {
-        'posts' : posts,
+        'posts' : Post.objects.all(),
         'title' : 'Home Page'
     }
     return render(request, 'blog/home.html', context)
