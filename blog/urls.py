12392701bd1925813a29dec440c03d8c2ebeeb8e
eduoblys <eduoblys@gmail.com>
2019-07-11 19:38:25 +0200

    pagination

--- blog/urls.py
+++ blog/urls.py
@@ -4,18 +4,21 @@ from .views import (
     PostDetailView, 
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    Contacts,
+    UserPostListView
 )
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 
-    path('contacts/', views.contacts, name='blog-contacts'),
+    path('contacts/', Contacts.as_view(), name='blog-contacts'),
 ]
 
