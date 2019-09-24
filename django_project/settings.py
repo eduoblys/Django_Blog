9da6d5d393c0204fd013de91e389206e2d3cc69a
eduoblys <eduoblys@gmail.com>
2019-09-24 14:03:31 +0200

    another heroku push

--- django_project/settings.py
+++ django_project/settings.py
@@ -139,7 +139,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'        #django-storages Amazon S3 docs
 
 django_heroku.settings(locals())
